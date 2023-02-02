87bb160225c819a8959d9e490a8fea22d3868e71
lnay <luke@grahamnaylor.net>
2023-02-02 01:45:08 +0000

    S H A M E correctly

--- main.rb
+++ main.rb
@@ -11,11 +11,28 @@ puts "The channel ID is #{channel_id}"
 
 bot = Discordrb::Bot.new token: bot_token
 
+def shame(message)
+  reactions = ['🇸', '🇭', '🇦', '🇲', '🇪', '👹']
+  reactions.each do |reacc|
+    message.create_reaction reacc
+  end
+
+  sleep 10
+  message.delete
+end
+
 rgx = /^(?<location>jcmb|bayes)(\s+[a-z0-9_\-:\.]+)?[!?]?$/i;
 
 bot.message() do |event|
-  event.respond "S H A M E" unless event.content =~ rgx
-  event.respond "Location: #{$~['location']}" if $~
+  # event.respond "S H A M E" unless event.content =~ rgx
+  # event.respond "Location: #{$~['location']}" if $~
+  match_data = rgx.match(event.content)
+  if match_data
+    location = match_data['location']
+    event.respond "Location: #{location}"
+  else
+    shame event.message
+  end
 end
 
 bot.run
