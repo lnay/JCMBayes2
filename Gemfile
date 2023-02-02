6e1a7f0b4e1136fde06470554d5df89f41bd5d45
lnay <luke@grahamnaylor.net>
2023-02-02 19:28:49 +0000

    Generate PNG file mockup

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'discordrb'
 gem 'yaml'
+gem 'rmagick'
--- gen_attendee_table_img.rb
+++ gen_attendee_table_img.rb
@@ -1,4 +1,5 @@
 require "erb"
+require "RMagick2"
 
 svg_template = ERB.new File.read "./TableTemplate.svg.erb"
 
@@ -6,4 +7,9 @@ attendee1 = "Luke"
 attendee2 = "Hannah"
 attendee3 = "Patrick"
 
-puts svg_template.result binding
+img = Magick::Image.from_blob(svg_template.result binding) {
+  format= 'SVG'
+  background_color='#2c2f33' # from discord colour palette
+}
+
+img[0].write "out.png"
