4f94e2ff224779daf4bd6b3c03740f8c8348b4f6
lnay <luke@grahamnaylor.net>
2023-02-03 00:17:31 +0000

    Cut down on main to get under 50 lines

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 require 'discordrb'
-require 'yaml'
 require "erb"
 require "RMagick2"
+require 'yaml'
 
 auth = YAML.load_file("auth.yml")
-
 bot_token = auth["token"]
 channel_id = auth["channel"]
 
@@ -13,49 +12,34 @@ puts "The channel ID is #{channel_id}"
 
 bot = Discordrb::Bot.new token: bot_token
 
-locations = ["jcmb", "bayes"]
-
-# hash associating each location to an empty list
-# (to later be filled with attendee names)
-people = locations.map{ |loc| [loc, []] }.to_h
+locations = ["JCMB", "Bayes"]
+# hash associating each location to an empty list (to fill with attendees)
+people = locations.map{ |loc| [loc.downcase, []] }.to_h
 
 def shame(message)
-  reactions = ['🇸', '🇭', '🇦', '🇲', '🇪', '👹']
-  reactions.each do |reacc|
-    message.create_reaction reacc
-  end
-
+  ['🇸', '🇭', '🇦', '🇲', '🇪', '👹'].each &message.method(:create_reaction)
   sleep 10
   message.delete
 end
 
 def gen_table_img(people, filename)
   svg_template = ERB.new File.read "./TableTemplate.svg.erb"
-
-  img = Magick::Image.from_blob(svg_template.result binding) {
-    format= 'SVG'
-  }
-
+  img = Magick::Image.from_blob(svg_template.result binding) { format='SVG' }
   img[0].write filename
 end
 
 permissible_message = /^
-  (?<location>#{locations.join("|")}) # Message must start with one location
-  (\s+[a-z0-9_\-:\.]+)? # Followed by optional extra (starting with a space)
+  (?<location>#{locations.join("|")}) # start with one location
+  (\s+[a-z0-9_\-:\.]+)? # Followed by optional extra, separated by a space
   [!?]? # End with optional punctuation
 $/ix
 
 bot.message() do |event|
-  # event.respond "S H A M E" unless event.content =~ rgx
-  # event.respond "Location: #{$~['location']}" if $~
-  match_data = permissible_message.match(event.content)
-  if match_data
-    location = match_data['location']
-    puts "Location: #{location}"
+  if event.content =~ permissible_message
+    location = $~['location']
     name = event.author.display_name
     location_attendees = people[location.downcase]
     location_attendees << name unless location_attendees.include?(name)
-    puts people
     gen_table_img people, "out.png"
     event.attach_file File.open("out.png", "r")
   else
