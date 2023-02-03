c47aa4b9e37a3ea198675fab7a8bea6257198321
lnay <luke@grahamnaylor.net>
2023-02-03 01:03:59 +0000

    Deal with global vars properly

--- main.rb
+++ main.rb
@@ -12,15 +12,15 @@ puts "The channel ID is #{CHANNEL_ID}"
 
 LOCATIONS = ["JCMB", "Bayes"]
 # Hash associating each location to an empty list (to fill with attendees)
-people = LOCATIONS.map{ |loc| [loc.downcase, []] }.to_h
+$people = LOCATIONS.map{ |loc| [loc.downcase, []] }.to_h
 
-def shame(message)
+def shame message
   ['🇸', '🇭', '🇦', '🇲', '🇪', '👹'].each &message.method(:create_reaction)
   sleep 10
   message.delete
 end
 
-def gen_table_img(people, filename)
+def gen_table_img filename
   svg_template = ERB.new File.read "./TableTemplate.svg.erb"
   img = Magick::Image.from_blob(svg_template.result binding) { format="SVG" }
   img[0].write filename
@@ -38,8 +38,8 @@ bot.message() do |event|
   if event.content =~ PERMISSIBLE_MESSAGE
     location = $~["location"].downcase
     name = event.author.display_name
-    people[location] << name unless people[location].include?(name)
-    gen_table_img people, "out.png"
+    $people[location] << name unless $people[location].include?(name)
+    gen_table_img "out.png"
     event.attach_file File.open("out.png", "r")
   else
     shame event.message
